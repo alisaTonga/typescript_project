f64b1891b98f1de494d30976f068ceae1d682ea1
alisaTonga <tongalise@gmail.com>
2024-07-12 09:25:11 +0200

    changed login form.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import Homework5 from './homework/homework05/homework5';
+import LoginForm from './components/loginForm/LoginForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <>
-  <Homework5 />
+  <LoginForm />
   </>
 );
 
